495fb34d6e0c030e3f86cebb7731a918ba8bd0ba
dola4 <126487707+dola4@users.noreply.github.com>
2024-01-30 12:18:29 -0500

    Update mongoDB.py

--- IA_pretest/database/mongoDB.py
+++ IA_pretest/database/mongoDB.py
@@ -2,7 +2,6 @@ from pymongo import MongoClient
 
 
 def connection():
-    #MONGODB_URI = "mongodb+srv://matpharma:srPiwY64u8HrT1sP@cluster0.rnzfdzw.mongodb.net/?retryWrites=true&w=majority"
     MONGODB_URI = "mongodb://localhost:27017/"
     client = MongoClient(MONGODB_URI)
     db = client['pharmaai']  
